f89fa69d66438b6340169a3dc90b35edf7f04512
Steve Sanbeg <steve@hpunr.(none)>
2012-07-31 18:57:46 -0400

    convert java to be compatable with fb text -w XSL; start looking for matches

--- Scan.java
+++ Scan.java
@@ -12,7 +12,7 @@ public class Scan
     {
 	public long uid=0;
 	public String username="";
-	public String name="";
+	public String name=null;
 	public String first_name="";
 	public String last_name="";
 	public String picture="";
@@ -32,8 +32,8 @@ public class Scan
     };
     
     
-
-    public static void main(String [] args)
+    private static ArrayList<Entry> scan_file(String file)
+	throws java.io.IOException, java.io.FileNotFoundException
     {
 	BufferedReader reader = null;
 	ArrayList<Entry> list = new ArrayList<Entry>();
@@ -42,7 +42,7 @@ public class Scan
 	Pattern pattern = Pattern.compile("^([0-9]*)\\s+(\\w+)\\s+(.+)");
 	
 	try {
-	    reader = new BufferedReader(new FileReader(args[0]));
+	    reader = new BufferedReader(new FileReader(file));
 	    
 	    String s;
 	    reader.readLine(); //first line is useless heading.
@@ -50,6 +50,7 @@ public class Scan
 	    while ((s=reader.readLine()) != null){
 		Matcher matcher = pattern.matcher(s);
 		if (matcher.matches()) {
+		    //System.err.println("got line");
 		    
 		    if (! matcher.group(1).equals("")){
 			cur = new Entry();
@@ -58,7 +59,8 @@ public class Scan
 
 		    String key = matcher.group(2);
 		    String val = matcher.group(3);
-
+		    //System.err.println("kv = " + key + " = " + val);
+		    
 		    if(key.equals("uid"))
 			cur.uid = Long.parseLong(val);
 		    else if (key.equals("name"))
@@ -71,23 +73,74 @@ public class Scan
 			cur.username = val;
 		    else if (key.equals("pic_big"))
 			cur.picture = val;
+		    //System.err.println("EOL");
+		    
 		}
 		
 		
 	    }
 	    
-	    
+	}
+	finally {
 	    if (reader != null)
 		reader.close();
-
-	    for (Entry ent : list ) {
-		System.out.println(ent);
-	    }
-
+	}
+	return list;
+	
+    };
+    
+    
+    public static void main(String [] args)
+    {
+	ArrayList<Entry> list = null, other_list=null;
+	try {
+	    list = scan_file(args[0]);
+	    if (args.length > 1)
+		other_list = scan_file(args[1]);
+	    
 	}
 	
 	catch (Exception e) {
 	    System.err.println( e.getMessage() );
+	}
+	
+	if (other_list == null) {
+	    for (Entry ent : list ) {
+		if (ent.name != null)
+		    System.out.println(ent);
+	    }
+	}
+	
+	if (other_list != null) {
+	    for (Entry fb_ent : list) {
+		if (fb_ent.name == null) continue;
+		for (Entry g_ent : other_list) {
+		    if (g_ent.name == null) continue;
+		    if (g_ent.name.equals(fb_ent.name)) {
+			//linkedin may have first last (LinkedIn...)
+			//probably nothing like that in fb friends, though
+			System.out.println(fb_ent.name);
+		    
+			/*
+			 *gmail may have combined them, so copy fb names
+			 * to gmail.
+			 */
+			if (fb_ent.first_name.equals(g_ent.first_name) &&
+			    fb_ent.last_name.equals(g_ent.last_name))
+			    //System.out.println("sn:"+fb_ent.name);
+			    ;
+			else {
+			    System.out.println(g_ent.first_name + " - " + g_ent.last_name);
+			}
+
+			if ("".equals(g_ent.picture))
+			    System.out.println("  need pic");
+			
+
+		    }
+		    
+		}
+	    }
 	}
 	
     }
---
+++ fblike.xsl
@@ -0,0 +1,42 @@
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+<xsl:output method="text"
+            doctype-public="-//W3C//DTD HTML 4.0//EN"/>
+
+<xsl:template match="contactlist">
+  <xsl:apply-templates select="contacts" />
+</xsl:template>
+
+<xsl:template match="contacts">
+  <xsl:text>fake	field	value
+</xsl:text>
+  <xsl:apply-templates select="contact"/>
+</xsl:template>
+
+<xsl:template match="contact">
+  <xsl:text>0	fake	thing</xsl:text>
+  <xsl:apply-templates/>
+</xsl:template>
+
+<xsl:template match="fn">
+  <xsl:text>	name	</xsl:text><xsl:value-of select="."/>
+</xsl:template>
+
+<xsl:template match="photo">
+  <xsl:text>	pic_big	</xsl:text><xsl:value-of select="@href"/>
+</xsl:template>
+
+<xsl:template match="name"><xsl:apply-templates/></xsl:template>
+
+<xsl:template match="given">
+  <xsl:text>	first_name	</xsl:text><xsl:value-of select="."/>
+</xsl:template>
+<xsl:template match="family">
+  <xsl:text>	last_name	</xsl:text><xsl:value-of select="."/>
+</xsl:template>
+
+<xsl:template match="*"></xsl:template>
+
+</xsl:stylesheet>
+		
+
