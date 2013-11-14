3a8bd7e155bad2bb32fe07056ce65ed5248c73a2
Steve Sanbeg <ssanbeg@users.sourceforge.net>
2013-11-14 16:15:26 -0500

    add match rultes

--- src/my/MergeEntries.java
+++ src/my/MergeEntries.java
@@ -34,4 +34,43 @@ public class MergeEntries
 	    rv.set_picture(fb.picture());
     }
     
+    public static String loose_name_match(EntryIF fb_ent, EntryIF g_ent) 
+    {
+
+	//exact match on first/last
+	if (g_ent.first_name.equals(fb_ent.first_name) && g_ent.last_name.equals(fb_ent.last_name)) {
+	    return "First/last";
+	}
+	//try splitting fb_ent.last, matching there
+
+	//if no last name, try first only?
+	else if (g_ent.last_name.equals("") 
+		 && 
+		 g_ent.first_name.equals(fb_ent.first_name)) {
+	    return "First only";
+	}
+	//also try split on "-"?
+	else if (fb_ent.last_name.contains(" ") 
+		 &&
+		 fb_ent.first_name.equals(g_ent.first_name)) {
+	    String names[] = fb_ent.last_name.split(" ");
+	    if (
+		names[names.length-1].equals(g_ent.last_name)
+		)			
+		return "Split";
+	    else
+		return null; //FIXME
+	}
+	else if (g_ent.last_name.equals(fb_ent.last_name)
+		 &&
+		 g_ent.first_name.charAt(0) == fb_ent.first_name.charAt(0)) {
+	    return "Initial";
+	}
+	else if (g_ent.first_name.equals(fb_ent.first_name)) {
+	    //System.out.println("First match: " + g_ent.name + "=" + fb_ent.name);
+	}
+	return null;
+    }
+    
+
 }
